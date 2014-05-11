6455759443e2c63c9c32bc96e1caf34e85b30b9d
Alessandro <hello@alessndro.co.uk>
2014-05-11 14:37:07 +0100

    Vanilla JS plugin implemented

---
+++ README.md
@@ -0,0 +1,4 @@
+# Page Scroll Indicator
+
+An simple JS plugin to display a small, fixed progress bar on the page that
+tracks how far the user has scrolled on a page.
---
+++ js/vanilla/page_scroll_indicator.js
@@ -0,0 +1,35 @@
+var PageScrollIndicator = PageScrollIndicator || {};
+
+PageScrollIndicator.createProgressBar = function(progressBarLocation, contentToTrack) {
+  
+  // Create the container div
+  var progressContElement = document.createElement("div");
+  progressContElement.id = "progress-cont";
+
+  // Create the progress bar itself
+  var progressBarElement = document.createElement("div");
+  progressBarElement.id = "progress-bar";
+  progressBarElement.style.width = "0%";
+
+  progressContElement.appendChild(progressBarElement);
+  
+  var locationObject = document.getElementById(progressBarLocation);
+  
+  if (locationObject.children === undefined) {
+    locationObject.appendChild(progressContElement);
+  } else {
+    locationObject.insertBefore(progressContElement, locationObject.firstChild);  
+  }
+
+  // Event handler that updates the width of the progress bar based
+  // on how far the contentToTrack elemt has been scrolled
+  window.onscroll = function(event) {
+    var pageHeight = window.innerHeight;
+    var container = document.getElementById(contentToTrack);
+    var adjHeight = container.clientHeight - pageHeight;
+
+    var progress = ((window.pageYOffset / adjHeight) * 100);
+
+    progressBarElement.style.width = progress + "%";
+  }
+}
